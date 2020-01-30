3c89c8fd5fa58625ff07f53d92ead6058a3c49ca
Pablo <p_felipe10@hotmail.com>
2020-01-30 16:37:09 -0200

    Finalizar paginas estáticas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'paginas_estaticas/incio'
   get 'paginas_estaticas/ajuda'
+ get 'paginas_estaticas/sobre'
   root 'application#ola'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
