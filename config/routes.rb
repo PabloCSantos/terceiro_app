5b5f1f61cfd0e912861866f3772532b1421133b9
Pablo <p_felipe10@hotmail.com>
2020-02-06 19:06:24 -0200

    Adicionar título dinâmico nsa páginas estáticas

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'paginas_estaticas/incio'
   get 'paginas_estaticas/ajuda'
  get 'paginas_estaticas/sobre'
+ get 'paginas_estaticas/contato'
   root 'application#ola'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
